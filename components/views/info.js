33f7a3621f8b0cdf9e4aea78ea454469d04afba7
Hsiao shen <Hsiao_shen@outlook.com>
2017-07-26 20:02:50 +0800

    new file:   components/index.js

---
+++ components/views/info.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Button}  from 'react-bootstrap';
+
+class Info extends Component {
+  render(){
+    return (
+      <div>
+        <Button bsStyle="primary">OK</Button>
+      </div>
+    )
+  }
+
+}
+
+module.exports = Info;
